77b5b39ade0a8231febffc3be4f0465b2068fce1
matnavarroc <81382843+matnavarroc@users.noreply.github.com>
2021-07-08 16:25:37 -0400

    Update settings.py

--- py/settings.py
+++ py/settings.py
@@ -1,6 +1,15 @@
 #Los Settings
 INSTALLED_APPS = [
 # agregar
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'core',
+    'rest_framework',
+    'rest_api',
     'rest_framework.authtoken',
 ]
 
