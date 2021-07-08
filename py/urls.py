5d02fc59acff106fceb3f4b2f52e3ba698c22c9e
matnavarroc <81382843+matnavarroc@users.noreply.github.com>
2021-07-08 15:12:18 -0400

    Update urls.py
    modificando los urls

--- py/urls.py
+++ py/urls.py
@@ -1,1 +1,16 @@
+#las URLS para importar algun que otro objeto
+from django.urls import path
+from .views import ropa-hombre, ropa-hombres, ropa-mujer, ropa-mujeres, ropa-ninas, ropa-nina, ropa-ninos, ropa-nino
+from .viewsLogin import login
 
+urlpatterns = [
+  path('ropa hombre/', ropa-hombres, name="ropa de hombres"),
+  path('ropa hombre/<pk>', ropa-hombre, name="ropa de hombre"),
+  path('ropa mujer/', ropa-mujeres, name="ropa de mujeres"),
+  path('ropa mujer/<pk>', ropa-mujer, name="ropa de mujer"),
+  path('ropa nina/', ropa-ninas, name="ropa de niñas"),
+  path('ropa nina/<pk>', ropa-nina, name="ropa de niña"),
+  path('ropa nino/', ropa-ninos, name="ropa de niños"),
+  path('ropa nino/<pk>', ropa-nino, name="ropa de niño"),
+  path('login/', login, name='login'),
+]
