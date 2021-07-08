437fe5bb93fdde149e50f0ce9676575e1ea31869
matnavarroc <81382843+matnavarroc@users.noreply.github.com>
2021-07-08 16:22:20 -0400

    Update settings.py

--- py/settings.py
+++ py/settings.py
@@ -1,1 +1,11 @@
+#Los Settings
+INSTALLED_APPS = [
+# agregar
+    'rest_framework.authtoken',
+]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
