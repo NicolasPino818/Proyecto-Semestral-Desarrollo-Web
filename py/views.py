c5a317677f5b73cd5f3ef2a040f15d0c13a767bb
matnavarroc <81382843+matnavarroc@users.noreply.github.com>
2021-07-08 14:44:48 -0400

    Update views.py

--- py/views.py
+++ py/views.py
@@ -1,1 +1,7 @@
+#Importaciones
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
+#Decoradores
+@permission_classes((IsAuthenticated,))
