1744210bb81cc5bc30f9bb3f12e21e6449556c93
matnavarroc <81382843+matnavarroc@users.noreply.github.com>
2021-07-14 15:47:04 -0400

    Update views.py

--- py/views.py
+++ py/views.py
@@ -1,106 +1,45 @@
 #Api de los objetos
-@api_view(['GET', 'PUT', 'DELETE'])
-def ropa-hombre(request, pk):
-  try:
-     ropa-hombre = ropa-hombre.objects.get(talla=pk)
-  except:
-    return Response(status=status.HTTP_404_NOT_FOUND)
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def productos(request, pk):
+      try:
+            productos = productos.objects.get(ropa.pk)
+      except:
+            return response(status=status.HTTP_404_NOT_FOUND)
 
-  if request.method == 'GET':
-        serializer = Ropa-HombreSerializer(Ropa-Hombre)
-        return Response(serializer.data)
-  
-  elif request.method == 'PUT':
-      data = JSONParser().parse(request)
-      serializer = Ropa-HombreSerializer(Ropa-Hombre, data=data)
-      if serializer.is_valid():
-            serializer.save()
+      if request.method == 'GET':
+            {
+                  "id": "<ID_PRODUCTO>", 
+                  "summary": "<tipo de producto>",
+                  "description" : "<Cualquier tipo de producto>"
+            }
+
+      elif request.method == 'POST':
+            serializer = ProductosSerializer(productos)
             return Response(serializer.data)
+
+      elif request.method == 'PUT':
+            data = JSONParser().parse(request)
+            serializer = ProductosSerializer(Productos, data=data)
+            if serializer.is_valid():
+                  serializer.save()
+                  return Response(serializer.data)
+            else:
+                  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+      elif request.method == 'DELETE':
+            productos.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
       else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-   elif request.method == 'DELETE':
-      ropa-hombre.delete()
-      return Responde(status=status.HTTP_204_NO_CONTENT)
-   else:
-      return response(status=status.HTTP_401_UNAUTHORIZED)
-  
- def ropa-mujer(request, pk):
-  try:
-     ropa-mujer = ropa-mujer.objects.get(talla=pk)
-  except:
-    return Response(status=status.HTTP_404_NOT_FOUND)
+            return Response(status=status.HTTP_401_UNAUTHORIZED)           
 
-  if request.method == 'GET':
-        serializer = Ropa-MujerSerializer(Ropa-Mujer)
-        return Response(serializer.data)
-  
-  elif request.method == 'PUT':
-      data = JSONParser().parse(request)
-      serializer = Ropa-MujerSerializer(Ropa-Mujer, data=data)
-      if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-      else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-   elif request.method == 'DELETE':
-      ropa-mujer.delete()
-      return Responde(status=status.HTTP_204_NO_CONTENT)
-   else:
-      return response(status=status.HTTP_401_UNAUTHORIZED) 
- 
-
-def ropa-nina(request, pk):
-  try:
-     ropa-hombre = ropa-nina.objects.get(talla=pk)
-  except:
-    return Response(status=status.HTTP_404_NOT_FOUND)
-
-  if request.method == 'GET':
-        serializer = Ropa-NinaSerializer(Ropa-Nina)
-        return Response(serializer.data)
-  
-  elif request.method == 'PUT':
-      data = JSONParser().parse(request)
-      serializer = Ropa-NinaSerializer(Ropa-Nina, data=data)
-      if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-      else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-   elif request.method == 'DELETE':
-      ropa-nina.delete()
-      return Responde(status=status.HTTP_204_NO_CONTENT)
-   else:
-      return response(status=status.HTTP_401_UNAUTHORIZED)
-
-def ropa-nino(request, pk):
-  try:
-     ropa-nino = ropa-nino.objects.get(talla=pk)
-  except:
-    return Response(status=status.HTTP_404_NOT_FOUND)
-
-  if request.method == 'GET':
-        serializer = Ropa-HombreSerializer(Ropa-Nino)
-        return Response(serializer.data)
-  
-  elif request.method == 'PUT':
-      data = JSONParser().parse(request)
-      serializer = Ropa-NinoSerializer(Ropa-Nino, data=data)
-      if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-      else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-   elif request.method == 'DELETE':
-      ropa-nino.delete()
-      return Responde(status=status.HTTP_204_NO_CONTENT)
-   else:
-      return response(status=status.HTTP_401_UNAUTHORIZED)
-    
 #Importaciones
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+import requests
+
+resp = requests.get('productos.html')
+if resp.status_code != 200:
+      raise ApiError('GET{}').format(resp.status_code))
+for todo_item in resp.json():
+      print('{} {}'.format(todo_item['id'], todo_item['summary']))      
 
 #Decoradores
 @permission_classes((IsAuthenticated,))
