0e450dd23d5333438bfbf535c9f025461b47a2d6
matnavarroc <81382843+matnavarroc@users.noreply.github.com>
2021-07-14 15:47:42 -0400

    Update urls.py

--- py/urls.py
+++ py/urls.py
@@ -1,16 +1,17 @@
 #las URLS para importar algun que otro objeto
 from django.urls import path
-from .views import ropa-hombre, ropa-hombres, ropa-mujer, ropa-mujeres, ropa-ninas, ropa-nina, ropa-ninos, ropa-nino
+from .views import productos, producto
 from .viewsLogin import login
+from . import views
 
 urlpatterns = [
-  path('ropa hombre/', ropa-hombres, name="ropa de hombres"),
-  path('ropa hombre/<pk>', ropa-hombre, name="ropa de hombre"),
-  path('ropa mujer/', ropa-mujeres, name="ropa de mujeres"),
-  path('ropa mujer/<pk>', ropa-mujer, name="ropa de mujer"),
-  path('ropa nina/', ropa-ninas, name="ropa de niñas"),
-  path('ropa nina/<pk>', ropa-nina, name="ropa de niña"),
-  path('ropa nino/', ropa-ninos, name="ropa de niños"),
-  path('ropa nino/<pk>', ropa-nino, name="ropa de niño"),
-  path('login/', login, name='login'),
+  path('Proyecto-semestral-desarollo-web/productos.html/', views.productos.html),
+  path('royecto-semestral-desarollo-web/nosotros.html/', views.nosotros.html),
+  path('royecto-semestral-desarollo-web/ropa-hombre.html/', views.ropahombre.html),
+  path('royecto-semestral-desarollo-web/ropa-mujer.html/', views.ropamujer.html),
+  path('royecto-semestral-desarollo-web/ropa-nina.html/', views.ropanina.html),
+  path('royecto-semestral-desarollo-web/ropa-nino.html/', views.ropanino.html),
+  path('producto/', productos, name="productos"),
+  path('productos/<pk>', producto, name="producto"),
+  path('login/', login, name='login')
 ]
